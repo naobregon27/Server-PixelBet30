90d120fddd4b351bc9fdd0519f9188c157f3d1fd
Laura <lauraahora4632025@gmail.com>
2025-06-21 18:02:09 -0300

    ultimos cambios alee 2

--- index.js
+++ index.js
@@ -138,28 +138,6 @@ app.post("/verificacion", async (req, res) => {
 
         try {
 
-          const PIXEL_ID = registro.pixel
-
-          // 🧠 Disparar el Pixel de Meta
-          !function (f, b, e, v, n, t, s) {
-            if (f.fbq) return;
-            n = f.fbq = function () {
-              n.callMethod ? n.callMethod.apply(n, arguments) : n.queue.push(arguments);
-            };
-            if (!f._fbq) f._fbq = n;
-            n.push = n;
-            n.loaded = !0;
-            n.version = '2.0';
-            n.queue = [];
-            t = b.createElement(e); t.async = !0;
-            t.src = v;
-            s = b.getElementsByTagName(e)[0];
-            s.parentNode.insertBefore(t, s)
-          }(window, document, 'script', 'https://connect.facebook.net/en_US/fbevents.js');
-
-          fbq('init', PIXEL_ID);
-          fbq('track', 'PageView');
-
           const pixelResponse = await axios.post(
             `https://graph.facebook.com/v19.0/${registro.pixel}/events`,
             {
