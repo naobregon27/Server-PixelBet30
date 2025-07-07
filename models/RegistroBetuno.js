b6d4b4fdb9102796e3b206b57f3228b8184cf29d
Laura <lauraahora4632025@gmail.com>
2025-07-07 12:26:26 -0300

    ingles2

--- models/RegistroBetuno.js
+++ models/RegistroBetuno.js
@@ -66,4 +66,4 @@ const registroSchema = new mongoose.Schema({
     }
 });
 
-module.exports = mongoose.model('RegistroBet30uno', registroSchema);
+module.exports = mongoose.model('RegistroBetuno', registroSchema);
